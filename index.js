a9aff30a8dac2a8bad2c1cc332569e68512b6f37
Nicole Sevillano <34521909+s3vi26@users.noreply.github.com>
2021-03-12 18:54:38 -0500

    added dotenv configs

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
@@ -5,7 +6,7 @@ const nodemailer = require("nodemailer");
 
 const { USER, PASS, MY_EMAIL } = process.env;
 
-app.post('/send', async function (req, res) {
+app.post('/sendEmail', async function (req, res) {
   console.log(req);
 
   const { name, title, email, phone, link, description } = req.body;
@@ -13,7 +14,7 @@ app.post('/send', async function (req, res) {
   const transporter = nodemailer.createTransport({
     host: "smtp-mail.outlook.com",
     port: 587,
-    secure: false, // true for 465, false for other ports
+    secure: false,
     auth: {
       user: USER,
       pass: PASS,
@@ -21,15 +22,15 @@ app.post('/send', async function (req, res) {
   });
 
   const info = await transporter.sendMail({
-    from: `${email}`, // sender address
-    to: MY_EMAIL, // list of receivers
-    subject: "Sevi-Codes.io/Contact", // Subject line
-    text: `This email is from ${name} - ${title}. Their contact number is ${phone}. Job posting link is here ${link}. Here is what they communicated about the job: ${description}`, // plain text body
+    from: `${email}`,
+    to: MY_EMAIL,
+    subject: "Sevi-Codes.io/Contact",
+    text: `This email is from ${name} - ${title}. Their contact number is ${phone}. Job posting link is here ${link}. Here is what they communicated about the job: ${description}`,
   });
 
   console.log(info);
 
   res.send('Email Sent!');
-})
+});
 
 module.exports.handler = serverless(app);
