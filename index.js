8703ffb6ceb99f09af94938b27da7bbbace31799
Nicole Sevillano <34521909+s3vi26@users.noreply.github.com>
2021-03-13 13:26:37 -0500

    minor changes and api in deploy

--- index.js
+++ index.js
@@ -7,9 +7,8 @@ const nodemailer = require("nodemailer");
 const { EMAIL_HOST, EMAIL_PORT, USER, PASS, MY_EMAIL } = process.env;
 
 app.post('/sendEmail', async function (req, res) {
-  console.log(req.body);
-  const { name, title, email, phone, link, description } = req.body;
-  console.log(name, title, email, phone, link, description)
+  const { name, title, email, phone, link, description } = JSON.parse(req.body);
+
   const transporter = nodemailer.createTransport({
     host: EMAIL_HOST,
     port: EMAIL_PORT,
@@ -20,16 +19,21 @@ app.post('/sendEmail', async function (req, res) {
     },
   });
 
-  const info = await transporter.sendMail({
-    from: `${email}`,
-    to: MY_EMAIL,
-    subject: "Sevi-Codes.io/Contact",
-    text: `This email is from ${name} - ${title}. Their contact number is ${phone}. Job posting link is here ${link}. Here is what they communicated about the job: ${description}`,
-  });
-
-  console.log(info);
-
-  res.send('Email Sent!');
+  console.log('connection established');
+  try {
+    const info = await transporter.sendMail({
+      to: MY_EMAIL,
+      subject: "Sevi-Codes.io/Contact",
+      text: `This email is from ${name} - ${title}. \n Their contact number is ${phone}; email is ${email}.\n Job posting link is here ${link}.\n Here is what they communicated about the job: ${description}`,
+    });
+  
+    console.log(info);
+  
+    res.send('Email Sent!');
+  } catch (e) {
+    console.log('error occured');
+    console.log(e);
+  }
 });
 
 module.exports.handler = serverless(app);
