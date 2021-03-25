4c7e9647c9efd38e7f284c5854534358f136cc60
Nicole Sevillano <34521909+s3vi26@users.noreply.github.com>
2021-03-25 12:04:54 -0400

    inital enpoint for retriving resume from s3

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
 const nodemailer = require("nodemailer");
+const AWS = require('aws-sdk');
 
-const { EMAIL_HOST, EMAIL_PORT, USER, PASS, MY_EMAIL } = process.env;
+const { ACCESS_KEY, SECRET_KEY, EMAIL_HOST, EMAIL_PORT, USER, PASS, MY_EMAIL } = process.env;
+
+AWS.Config({
+  accesssKeyId: ACCESS_KEY, 
+  secretAccessKey: SECRET_KEY, 
+  region: 'us-east-1'
+});
 
 app.post('/sendEmail', async function (req, res) {
   const body = req.body.toString('utf8');
@@ -35,6 +42,18 @@ app.post('/sendEmail', async function (req, res) {
     console.log('error occured');
     console.log(e);
   }
+});
+
+app.get('/docs/:resume', async function (req, res) {
+  console.log(req);
+  const { resume } = req.params;
+  const s3 = new AWS.S3({apiVersion: '2006-03-01'});
+  const params = {
+    Bucket: 'docs-personalz',
+    Key: resume
+  }
+  const resume = await s3.getObject(params);
+  console.log(resume);
 });
 
 module.exports.handler = serverless(app);
